ea5e072cd884356e6acf67edb8dc3931061a1fc9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-10 07:01:39 -0400

    download and use the excludelist and some refactoring

--- linux/deploy-linux.sh
+++ linux/deploy-linux.sh
@@ -15,77 +15,63 @@
 # can help reduce the end AppImage's size, although in a production system this script proved
 # unhelpful.
 #~ set -x
+command -v patchelf 1>/dev/null || { echo ERROR: missing patchelf; exit 1; }
+command -v wget 1>/dev/null || { echo ERROR: missing wget; exit 1; }
 export _PREFIX="/usr"
 export _LIB_DIRS="lib64 lib"
-export _QT_PLUGIN_PATH="${_PREFIX}/lib64/qt5/plugins"
-export _EXCLUDES="ld-linux.so.2 ld-linux-x86-64.so.2 libanl.so.1 libBrokenLocale.so.1 libcidn.so.1 \
-libc.so.6 libdl.so.2 libm.so.6 libmvec.so.1 libnss_compat.so.2 libnss_dns.so.2 libnss_files.so.2 \
-libnss_hesiod.so.2 libnss_nisplus.so.2 libnss_nis.so.2 libpthread.so.0 libresolv.so.2 librt.so.1 \
-libthread_db.so.1 libutil.so.1 libstdc++.so.6 libGL.so.1 libEGL.so.1 libGLdispatch.so.0 \
-libGLX.so.0 libOpenGL.so.0 libdrm.so.2 libglapi.so.0 libgbm.so.1 libxcb.so.1 libX11.so.6 \
-libasound.so.2 libfontconfig.so.1 libthai.so.0 libfreetype.so.6 libharfbuzz.so.0 libcom_err.so.2 \
-libexpat.so.1 libgcc_s.so.1 libgpg-error.so.0 libICE.so.6 libp11-kit.so.0 libSM.so.6 \
-libusb-1.0.so.0 libuuid.so.1 libz.so.1 libpangoft2-1.0.so.0 libpangocairo-1.0.so.0 \
-libpango-1.0.so.0 libgpg-error.so.0 libjack.so.0 libxcb-dri3.so.0 libxcb-dri2.so.0 \
-libfribidi.so.0 libgmp.so.10"
-
-export _EXECUTABLE="$1"
+[ -z "$_QT_PLUGIN_PATH" ] && export _QT_PLUGIN_PATH="${_PREFIX}/lib64/qt5/plugins"
+FORBIDDEN="https://raw.githubusercontent.com/AppImageCommunity/pkg2appimage/master/excludelist"
+export EXCLUDES="$(wget -q "$FORBIDDEN" -O - | sed 's/#.*//; /^$/d')"
+[ -z "$EXCLUDES" ] && exit 1
+export TARGET="$1"
 
 # Get possible system library paths
 export _SYSTEM_PATHS=$(echo -n $PATH | tr ":" " ")
-export _SEARCH_PATHS=
-for i in ${_LIB_DIRS}; do
-  for j in ${_SYSTEM_PATHS}; do
-    _TRY_PATH="$(readlink -e "$j/../$i" || true)"
-    if [[ -n "${_TRY_PATH}" ]]; then
-      _SEARCH_PATHS="${_SEARCH_PATHS} ${_TRY_PATH}"
-    fi
+export SEARCH_PATHS=
+for lib in ${_LIB_DIRS}; do
+  for path in ${_SYSTEM_PATHS}; do
+    _TRY_PATH="$(readlink -e "$path/../$lib" || true)"
+    [ -n "${_TRY_PATH}" ] && SEARCH_PATHS="${SEARCH_PATHS} ${_TRY_PATH}"
   done
 done
-_SEARCH_PATHS="${_SEARCH_PATHS} $(patchelf --print-rpath $_EXECUTABLE | tr ':' ' ')"
+SEARCH_PATHS="${SEARCH_PATHS} $(patchelf --print-rpath $TARGET | tr ':' ' ')"
 # Get a list of only unique ones
-_SEARCH_PATHS=$(echo -n "${_SEARCH_PATHS}" | sed 's/ /\n/g' | sort -u)
+SEARCH_PATHS=$(echo -n "${SEARCH_PATHS}" | sed 's/ /\n/g' | sort -u)
 
 # find_library [library]
-#  Finds the full path of partial name [library] in _SEARCH_PATHS
+#  Finds the full path of partial name [library] in SEARCH_PATHS
 #  This is a time-consuming function.
-_NOT_FOUND=""
-function find_library {
+NOT_FOUND=""
+find_library() {
   local _PATH=""
-  for i in ${_SEARCH_PATHS}; do
+  for i in ${SEARCH_PATHS}; do
     _PATH=$(find $i -regex ".*$(echo -n $1 | tr '+' '.')" -print -quit)
-    if [ "$_PATH" != "" ]; then
-      break
-    fi
+    [ -n "$_PATH" ] && break
   done
-  if [ "$_PATH" != "" ]; then
-    echo -n $(readlink -e $_PATH)
-  fi
+  [ -n "$_PATH" ] && readlink -e $_PATH | tr ':' ' '
 }
 
 # get_dep_names [object]
 #  Returns a space-separated list of all required libraries needed by [object].
-function get_dep_names {
-  echo -n $(patchelf --print-needed $1)
+get_dep_names() {
+  patchelf --print-needed $1 | tr '\n' ' '
 }
 
 # get_deps [object] [library_path]
 #  Finds and installs all libraries required by [object] to [library_path].
 #  This is a recursive function that also depends on find_library.
-function get_deps {
+get_deps() {
   local _DEST=$2
   for i in $(get_dep_names $1); do
-    _EXCL=`echo "$_EXCLUDES" | tr ' ' '\n' | grep $i`
+    _EXCL=`echo "$EXCLUDES" | tr ' ' '\n' | grep $i`
     if [ "$_EXCL" != "" ]; then
-      #>&2 echo "$i is on the exclude list... skipping"
+      #printf '\n%s\n' "$i is on the exclude list... skipping"
       continue
     fi
-    if [ -f $_DEST/$i ]; then
-      continue
-    fi
+    [ -f $_DEST/$i ] && continue
     local _LIB=$(find_library $i)
     if [ -z $_LIB ]; then
-      echo -n "$i:"
+      echo "$i:"
       continue
     fi
     >&2 cp -v $_LIB $_DEST/$i &
@@ -98,25 +84,22 @@ export -f get_dep_names
 export -f find_library
 
 _ERROR=0
-if [ -z "$_EXECUTABLE" ]; then
-  _ERROR=1
-fi
-
+[ -z "$TARGET" ] && _ERROR=1
 if [ "$_ERROR" -eq 1 ]; then
-  >&2 echo "usage: $0 <executable> [AppDir]"
+  echo "USAGE: $0 </path/to/executable> [AppDir]"
   exit 1
 fi
 
-LIB_DIR="$(readlink -m $(dirname $_EXECUTABLE)/../lib)"
+LIB_DIR="$(readlink -m $(dirname $TARGET)/../lib)"
 mkdir -p $LIB_DIR
-_NOT_FOUND=$(get_deps $_EXECUTABLE $LIB_DIR)
+NOT_FOUND=$(get_deps $TARGET $LIB_DIR)
 
-if [ "${DEPLOY_QT}" == "1" ]; then
+if [ "$DEPLOY_QT" = "1" ]; then
   # Find Qt path from search paths
-  for i in ${_SEARCH_PATHS}; do
-    _QT_CORE_LIB=$(find ${i} -type f -regex '.*/libQt5Core\.so.*' | head -n 1)
-    if [ -n "${_QT_CORE_LIB}" ]; then
-      _QT_PATH=$(dirname ${_QT_CORE_LIB})/../
+  for lib in ${SEARCH_PATHS}; do
+    _QT_CORE_LIB=$(find $lib -type f -regex '.*/libQt5Core\.so.*' | head -n 1)
+    if [ -n "$_QT_CORE_LIB" ]; then
+      _QT_PATH=$(dirname $_QT_CORE_LIB)/../
       break
     fi
   done
@@ -126,14 +109,14 @@ if [ "${DEPLOY_QT}" == "1" ]; then
   mkdir -p ${LIB_DIR}/../plugins/platforms
   cp -nv "${_QT_PLUGIN_PATH}/platforms/libqxcb.so" ${LIB_DIR}/../plugins/platforms/
 	# Find any remaining libraries needed for Qt libraries
-  _NOT_FOUND+=$(get_deps ${LIB_DIR}/../plugins/platforms/libqxcb.so $LIB_DIR)
+  NOT_FOUND+=$(get_deps ${LIB_DIR}/../plugins/platforms/libqxcb.so $LIB_DIR)
 
   for i in audio bearer imageformats mediaservice platforminputcontexts platformthemes xcbglintegrations; do
     mkdir -p ${LIB_DIR}/../plugins/${i}
     cp -rnv ${_QT_PLUGIN_PATH}/${i}/*.so ${LIB_DIR}/../plugins/${i}
     find ${LIB_DIR}/../plugins/ -type f -regex '.*\.so' -exec patchelf --set-rpath '$ORIGIN/../../lib:$ORIGIN' {} ';'
     # Find any remaining libraries needed for Qt libraries
-    _NOT_FOUND+=$(find ${LIB_DIR}/../plugins/${i} -type f -exec bash -c "get_deps {} $LIB_DIR" ';')
+    NOT_FOUND+=$(find ${LIB_DIR}/../plugins/${i} -type f -exec bash -c "get_deps {} $LIB_DIR" ';')
   done
   
   _QT_CONF=${LIB_DIR}/../bin/qt.conf
@@ -146,12 +129,12 @@ fi
 
 # Fix rpath of libraries and executable so they can find the packaged libraries
 find ${LIB_DIR} -type f -exec patchelf --set-rpath '$ORIGIN' {} ';'
-patchelf --set-rpath '$ORIGIN/../lib' $_EXECUTABLE
+patchelf --set-rpath '$ORIGIN/../lib' $TARGET
 
 _APPDIR=$2
 cd ${_APPDIR}
 
-if [ "${_NOT_FOUND}" != "" ]; then
-  >&2 echo "WARNING: failed to find the following libraries:"
-  >&2 echo "$(echo -n $_NOT_FOUND | tr ':' '\n' | sort -u)"
+if [ -n "${NOT_FOUND}" ]; then
+  echo "WARNING: failed to find the following libraries:"
+  echo "$(pritnf $NOT_FOUND | tr ':' '\n' | sort -u)"
 fi
