7006e870f8b7b3d37f5a9fbdc7fa4d7ee9fe245a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-10 15:06:46 -0400

    refactoring part 3

--- linux/deploy-linux.sh
+++ linux/deploy-linux.sh
@@ -15,71 +15,94 @@
 # can help reduce the end AppImage's size, although in a production system this script proved
 # unhelpful.
 #~ set -x
-# safety checks
+
+# safety checks and set vars
 NOT_FOUND=""
-[ -z "$1" ] && echo "USAGE: $0 </path/to/executable> [AppDir]" && exit 1
 TARGET="$(command -v "$1" 2>/dev/null)"
 APPDIR="$2"
-[ -z $TARGET ] && echo "ERROR: missing $1" && exit 1
-! command -v patchelf 1>/dev/null && echo "ERROR: missing patchelf" && exit 1
-! command -v wget 1>/dev/null && echo "ERROR: missing wget" && exit 1
-export _PREFIX="/usr"
-export LIB_DIRS="lib64 lib"
-[ -z "$QT_PLUGIN_PATH" ] && export QT_PLUGIN_PATH="${_PREFIX}/lib64/qt5/plugins"
 FORBIDDEN="https://raw.githubusercontent.com/AppImageCommunity/pkg2appimage/master/excludelist"
-export EXCLUDES="$(wget -q "$FORBIDDEN" -O - | sed 's/#.*//; /^$/d')"
-[ -z "$EXCLUDES" ] && exit 1
-[ -z "$QT_PLUGIN_NAMES" ] && QT_PLUGIN_NAMES="audio bearer imageformats \
-    mediaservice platforminputcontexts platformthemes xcbglintegrations"
+EXCLUDES="$(wget -q "$FORBIDDEN" -O - | sed 's/#.*//; /^$/d')"
+if [ -z "$1" ]; then
+    echo "USAGE: $0 /path/to/binary"
+    echo "USAGE: $0 /path/to/binary /path/to/AppDir"
+    exit 1
+elif [ -z $TARGET ]; then
+    echo "ERROR: Missing \"$1\"!"
+    exit 1
+elif ! command -v patchelf 1>/dev/null; then
+    echo "ERROR: Missing patchelf dependency!"
+    exit 1
+elif ! command -v wget 1>/dev/null; then
+    echo "ERROR: Missing wget dependency!"
+    exit 1
+elif [ -z "$EXCLUDES" ]; then
+    echo "ERROR: Can't download the exclude list, no internet?"
+    exit 1
+fi
+[ -z "$PREFIX" ] && PREFIX="/usr"
+[ -z "$LIB_DIRS" ] && LIB_DIRS="lib64 lib"
+[ -z "$QT_PLUGIN_PATH" ] && QT_PLUGIN_PATH="$PREFIX/lib64/qt5/plugins"
+[ -z "$QT_PLUGINS" ] && QT_PLUGINS="audio bearer imageformats mediaservice \
+                                platforminputcontexts platformthemes \
+                                xcbglintegrations iconengines"
+export EXCLUDES
+export LIB_DIRS
+export PREFIX
+export QT_PLUGIN_PATH
 
-# Get possible system library paths
-export SYSTEM_PATHS=$(printf $PATH | tr ":" " ")
-export SEARCH_PATHS=
-for i in ${LIB_DIRS}; do
-  for j in ${SYSTEM_PATHS}; do
-    TRY_PATH="$(readlink -e "$j/../$i" || true)"
-    if [ -n "$TRY_PATH" ]; then
-      SEARCH_PATHS="$SEARCH_PATHS $TRY_PATH"
-    fi
-  done
-done
-SEARCH_PATHS="$SEARCH_PATHS $(patchelf --print-rpath $TARGET | tr ':' ' ')"
-# Get a list of only unique ones
-SEARCH_PATHS=$(printf "$SEARCH_PATHS" | sed 's/ /\n/g' | sort -u)
-
-# this is cursed omg
-_get_deps() {
-  local DESTDIR=$2
-  for i in $(patchelf --print-needed $1 | tr '\n' ' '); do
-    if echo "$EXCLUDES" | grep -q $i; then
-      >&2 echo "$i is on the exclude list... skipping"
-      continue
-    fi
-    [ -f $DESTDIR/$i ] && continue
-    for lib in $SEARCH_PATHS; do
-        _PATH=$(find $lib -regex ".*$(echo -n $i | tr '+' '.')" -print -quit)
-        [ -n "$_PATH" ] && break
+# Look for a lib dir next to each instance of PATH
+export LIB_PATHS=
+for libpath in $LIB_DIRS; do
+    for path in $(printf $PATH | tr ":" " "); do
+        TRY_PATH="$(readlink -e "$path/../$libpath" 2>/dev/null)"
+        [ -n "$TRY_PATH" ] && LIB_PATHS="$LIB_PATHS $TRY_PATH"
     done
-    _LIB=""
-    [ -n "$_PATH" ] && _LIB=$(readlink -e $_PATH | tr '\n' ' ')
-    if [ -z $_LIB ]; then
-        echo -n "$i:"
-        continue
-    fi
-    >&2 cp -v $_LIB $DESTDIR/$i &
-    _get_deps $_LIB $DESTDIR
-  done
+done
+# Include rpath of binary and only show unique
+LIB_PATHS="$LIB_PATHS $(patchelf --print-rpath $TARGET | tr ':' ' ')"
+LIB_PATHS=$(printf "$LIB_PATHS" | sed 's/ /\n/g' | sort -u)
+
+_get_deps() {
+    local DESTDIR=$2
+    local needed_libs=$(patchelf --print-needed $1 | tr '\n' ' ')
+    for i in $needed_libs; do
+        # check it isn't in the exclude list or it is already deployed
+        if echo "$EXCLUDES" | grep -q $i; then
+            >&2 echo "$i is on the exclude list... skipping"
+            continue
+        elif [ -f $DESTDIR/$i ]; then
+            >&2 echo "$i is already deployed... skipping"
+            continue
+        fi
+        _LIB=""
+        # this is cursed omg
+        for lib in $LIB_PATHS; do
+            _PATH=$(find $lib -regex ".*$(echo -n $i | tr '+' '.')" -print -quit)
+            if [ -n "$_PATH" ]; then
+                _LIB=$(readlink -e $_PATH | tr '\n' ' ') 
+                if [ -z $_LIB ]; then
+                    >&2 printf '\n%s\n\n' "ERROR: could not find \"$i\""
+                    echo -n "$i:"
+                    continue
+                fi
+                break
+            fi
+        done
+        # copy libs to appdir
+        >&2 cp -v $_LIB $DESTDIR/$i &
+        _get_deps $_LIB $DESTDIR
+    done
 }
 
-export -f _get_deps
+export -f _get_deps # TODO GET RID OF BASHISM
 
 LIB_DIR="$(readlink -m $(dirname $TARGET)/../lib)"
 mkdir -p $LIB_DIR
 NOT_FOUND=$(_get_deps $TARGET $LIB_DIR)
 
-if [ "${DEPLOY_QT}" == "1" ]; then
+if [ "${DEPLOY_QT}" = "1" ]; then
   # Find Qt path from search paths
-  for i in $SEARCH_PATHS; do
+  for i in $LIB_PATHS; do
     _QT_CORE_LIB=$(find ${i} -type f -regex '.*/libQt5Core\.so.*' | head -1)
     if [ -n "${_QT_CORE_LIB}" ]; then
       _QT_PATH=$(dirname ${_QT_CORE_LIB})/../
@@ -94,7 +117,7 @@ if [ "${DEPLOY_QT}" == "1" ]; then
 	# Find any remaining libraries needed for Qt libraries
   NOT_FOUND+=$(_get_deps ${LIB_DIR}/../plugins/platforms/libqxcb.so $LIB_DIR)
 
-  for i in $QT_PLUGIN_NAMES; do
+  for i in $QT_PLUGINS; do
     mkdir -p ${LIB_DIR}/../plugins/${i}
     cp -rnv ${QT_PLUGIN_PATH}/${i}/*.so ${LIB_DIR}/../plugins/${i}
     find ${LIB_DIR}/../plugins/ \
