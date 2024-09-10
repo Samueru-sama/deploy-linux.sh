be6b6bfbbfef4a717324b099cac404230c3e95a7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-09-10 05:10:51 -0400

    remove .desktop and icon find and copy

--- linux/deploy-linux.sh
+++ linux/deploy-linux.sh
@@ -151,11 +151,7 @@ patchelf --set-rpath '$ORIGIN/../lib' $_EXECUTABLE
 _APPDIR=$2
 cd ${_APPDIR}
 
-cp -nvs $(find -type f -regex '.*/icons/.*\.svg' || head -n 1) ./
-cp -nvs $(find -type f -regex '.*/applications/.*\.desktop' || head -n 1) ./
-
 if [ "${_NOT_FOUND}" != "" ]; then
   >&2 echo "WARNING: failed to find the following libraries:"
   >&2 echo "$(echo -n $_NOT_FOUND | tr ':' '\n' | sort -u)"
 fi
-
